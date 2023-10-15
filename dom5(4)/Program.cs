cfcef11dd0e799650ccab484a0f1b06bc25c1906
Арман <@LAPTOP-8BLD2IN5>
2023-10-15 23:01:17 +0600

    Добавьте файлы проекта.

---
+++ dom5(4)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            CallerMethod();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Исключение в вызывающем методе: {ex.Message}");
+        }
+    }
+
+    static void CallerMethod()
+    {
+        try
+        {
+            CalleeMethod();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Исключение в вызываемом методе: {ex.Message}");
+            throw; // Поднимаем исключение обратно в вызывающий метод
+        }
+    }
+
+    static void CalleeMethod()
+    {
+        throw new Exception("Генерируем исключение в вызываемом методе.");
+    }
+}
